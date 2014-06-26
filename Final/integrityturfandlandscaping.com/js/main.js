da24d71765da0507df97b52d976d372de75ad4b5
gipsonsa <gipsonsa@msu.edu>
2014-06-26 15:53:15 -0400

    Final Project integrityturfandlandscaping.com
    live at integrityturfandlandscaping.com

---
+++ Final/integrityturfandlandscaping.com/js/main.js
@@ -0,0 +1,231 @@
+/* ==========================================================================
+PATIOS, WALKWAYS, AND RETAINING WALLS
+   ========================================================================== */
+
+
+$(document).ready(function (){
+	$('.walk-image1').mouseover(function () {
+		$('#middle').css("background-image","url(img/walk/walk1b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.walk-image2').mouseover(function () {
+		$('#middle').css("background-image","url(img/walk/walk2b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.walk-image3').mouseover(function () {
+		$('#middle').css("background-image","url(img/walk/walk3b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.walk-image4').mouseover(function () {
+		$('#middle').css("background-image","url(img/walk/walk4b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.walk-image5').mouseover(function () {
+		$('#middle').css("background-image","url(img/walk/walk5b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.walk-image6').mouseover(function () {
+		$('#middle').css("background-image","url(img/walk/walk6b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.walk-image7').mouseover(function () {
+		$('#middle').css("background-image","url(img/walk/walk7b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.walk-image8').mouseover(function () {
+		$('#middle').css("background-image","url(img/walk/walk8b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.walk-image9').mouseover(function () {
+		$('#middle').css("background-image","url(img/walk/walk9b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.walk-image10').mouseover(function () {
+		$('#middle').css("background-image","url(img/walk/walk10b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.walk-image11').mouseover(function () {
+		$('#middle').css("background-image","url(img/walk/walk11b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.walk-image12').mouseover(function () {
+		$('#middle').css("background-image","url(img/walk/walk12b.jpg)");
+	});
+});
+
+
+/* ==========================================================================
+PLANTING AND DESIGNS
+   ========================================================================== */
+
+
+$(document).ready(function (){
+	$('.plant-image1').mouseover(function () {
+		$('#middle2').css("background-image","url(img/plant/plant1b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.plant-image2').mouseover(function () {
+		$('#middle2').css("background-image","url(img/plant/plant2b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.plant-image3').mouseover(function () {
+		$('#middle2').css("background-image","url(img/plant/plant3b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.plant-image4').mouseover(function () {
+		$('#middle2').css("background-image","url(img/plant/plant4b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.plant-image5').mouseover(function () {
+		$('#middle2').css("background-image","url(img/plant/plant5b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.plant-image6').mouseover(function () {
+		$('#middle2').css("background-image","url(img/plant/plant6b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.plant-image7').mouseover(function () {
+		$('#middle2').css("background-image","url(img/plant/plant7b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.plant-image8').mouseover(function () {
+		$('#middle2').css("background-image","url(img/plant/plant8b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.plant-image9').mouseover(function () {
+		$('#middle2').css("background-image","url(img/plant/plant9b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.plant-image10').mouseover(function () {
+		$('#middle2').css("background-image","url(img/plant/plant10b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.plant-image11').mouseover(function () {
+		$('#middle2').css("background-image","url(img/plant/plant11b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.plant-image12').mouseover(function () {
+		$('#middle2').css("background-image","url(img/plant/plant12b.jpg)");
+	});
+});
+
+/* ==========================================================================
+LAWN
+   ========================================================================== */
+
+
+$(document).ready(function (){
+	$('.lawn-image1').mouseover(function () {
+		$('#middle3').css("background-image","url(img/lawn/lawn1b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.lawn-image2').mouseover(function () {
+		$('#middle3').css("background-image","url(img/lawn/lawn2b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.lawn-image3').mouseover(function () {
+		$('#middle3').css("background-image","url(img/lawn/lawn3b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.lawn-image4').mouseover(function () {
+		$('#middle3').css("background-image","url(img/lawn/lawn4b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.lawn-image5').mouseover(function () {
+		$('#middle3').css("background-image","url(img/lawn/lawn5b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.lawn-image6').mouseover(function () {
+		$('#middle3').css("background-image","url(img/lawn/lawn6b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.lawn-image7').mouseover(function () {
+		$('#middle3').css("background-image","url(img/lawn/lawn7b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.lawn-image8').mouseover(function () {
+		$('#middle3').css("background-image","url(img/lawn/lawn8b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.lawn-image9').mouseover(function () {
+		$('#middle3').css("background-image","url(img/lawn/lawn9b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.lawn-image10').mouseover(function () {
+		$('#middle3').css("background-image","url(img/lawn/lawn10b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.lawn-image11').mouseover(function () {
+		$('#middle3').css("background-image","url(img/lawn/lawn11b.jpg)");
+	});
+});
+
+$(document).ready(function (){
+	$('.lawn-image12').mouseover(function () {
+		$('#middle3').css("background-image","url(img/lawn/lawn12b.jpg)");
+	});
+});
